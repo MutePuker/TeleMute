7ce5f4e79b9802d0388ae5bf1c10ae7527d596dc
MutePuker <mutepuker@gmail.com>
2016-12-28 18:45:42 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 wget "https://valtman.name/files/telegram-cli-1215"
+mv telegram-cli-1215 tg
 sudo apt-get install libreadline-dev libconfig-dev libssl-dev lua5.2 liblua5.2-dev lua-socket lua-sec lua-expat libevent-dev make unzip git redis-server autoconf g++ libjansson-dev libpython-dev expat libexpat1-dev ppa-purge python3-pip python3-dev
 sudo pip3 install redis
 sudo service redis-server restart
