c324b9c32ec41fde42a70b8deea73dc7d9d4a8dc
MutePuker <mutepuker@gmail.com>
2017-01-03 18:15:40 +0330

    Update bot.lua

--- bot.lua
+++ bot.lua
@@ -196,6 +196,25 @@ function tdcli_update_callback(data)
       if redis:get('lock_entg:'..chat_id) and input:match("[A-Z]") then
         tdcli.deleteMessages(chat_id, {[0] = msg.id_})
 	  end
+	   if input:match("^[#!/][Ll]ock number$") and is_sudo(msg) then
+       if redis:get('lock_numtg:'..chat_id) then
+        tdcli.sendMessage(chat_id, msg.id_, 1, '<b>Number posting is already locked</b>', 1, 'html')
+       else -- @MuteTeam
+        redis:set('lock_numtg:'..chat_id, true)
+        tdcli.sendMessage(chat_id, msg.id_, 1, '<b>number posting has been locked</b>', 1, 'html')
+      end
+      end 
+      if input:match("^[#!/][Uu]nlock number$") and is_sudo(msg) then
+       if not redis:get('lock_numtg:'..chat_id) then
+        tdcli.sendMessage(chat_id, msg.id_, 1, '<b>number posting is not locked</b>', 1, 'html')
+       else
+         redis:del('lock_numtg:'..chat_id)
+        tdcli.sendMessage(chat_id, msg.id_, 1, '<b>number posting has been unlocked</b>', 1, 'html')
+      end
+      end
+      if redis:get('lock_numtg:'..chat_id) and input:match("[1-9999]") then
+        tdcli.deleteMessages(chat_id, {[0] = msg.id_})
+	  end
       if input:match("^[#!/][Mm]ute all$") and is_sudo(msg) then
        if redis:get('mute_alltg:'..chat_id) then
         tdcli.sendMessage(chat_id, msg.id_, 1, '<b>Mute All is already on</b>', 1, 'html')
