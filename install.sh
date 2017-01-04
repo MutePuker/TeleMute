529b23e749943592f2a5866a9d0001e6a218166e
MutePuker <mutepuker@gmail.com>
2017-01-04 03:35:38 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,25 @@ wget "https://valtman.name/files/telegram-cli-1222"
 mv telegram-cli-1222 tg
 sudo apt-get install libreadline-dev libconfig-dev libssl-dev lua5.2 liblua5.2-dev libevent-dev make autoconf unzip git redis-server g++ libjansson-dev libpython-dev expat libexpat1-dev
 sudo apt-get install libreadline-dev libconfig-dev libssl-dev lua5.2 liblua5.2-dev libevent-dev make autoconf unzip git redis-server g++ libjansson-dev libpython-dev expat libexpat1-dev
+sudo apt-get install libreadline-dev
+sudo apt install libreadline-dev 
+sudo apt-get install libreadline6 
+sudo apt-get install libreadline6-dev
+sudo apt-get install libconfig-dev 
+sudo apt-get install libssl-dev 
+sudo apt-get install lua5.2 
+sudo apt-get install liblua5.2-dev 
+sudo apt-get install lua-socket 
+sudo apt-get install lua-sec 
+sudo apt-get install lua-expat 
+sudo apt-get install libevent-dev 
+sudo apt-get install redis-server 
+sudo apt-get install libjansson-dev 
+sudo apt-get install libpython-dev 
+sudo apt-get install expat libexpat1-dev 
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get dist-upgrade
 chmod 777 tg
 chmod 777 launch.sh
 RED='\033[0;31m'
