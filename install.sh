3b89b0dacf47baf4a2e5a61b9736b5c77b7f1dc0
MutePuker <mutepuker@gmail.com>
2017-01-07 00:41:06 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,22 @@
 #!/usr/bin/env bash
 wget "https://valtman.name/files/telegram-cli-1222"
 mv telegram-cli-1222 tg
+cd
+sudo apt-get update 
+sudo apt-get upgrade
+sudo apt-get install libreadline-dev libssl-dev lua5.2 liblua5.2-dev git make unzip redis-server curl libcurl4-gnutls-dev
+wget http://luarocks.org/releases/luarocks-2.2.2.tar.gz 
+tar zxpf luarocks-2.2.2.tar.gz 
+cd luarocks-2.2.2
+./configure; sudo make bootstrap 
+sudo luarocks install luasec 
+sudo luarocks install luasocket 
+sudo luarocks install redis-lua 
+sudo luarocks install lua-term 
+sudo luarocks install serpent
+sudo luarocks install dkjson 
+sudo luarocks install Lua-cURL 
+cd TeleMute
 sudo apt-get install libreadline-dev libconfig-dev libssl-dev lua5.2 liblua5.2-dev libevent-dev make autoconf unzip git redis-server g++ libjansson-dev libpython-dev expat libexpat1-dev
 sudo apt-get install libreadline-dev libconfig-dev libssl-dev lua5.2 liblua5.2-dev libevent-dev make autoconf unzip git redis-server g++ libjansson-dev libpython-dev expat libexpat1-dev
 sudo apt-get install libreadline-dev
