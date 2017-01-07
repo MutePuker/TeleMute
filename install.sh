341b973a41755b859f084d3119a82d77c7acd7e7
MutePuker <mutepuker@gmail.com>
2017-01-08 03:06:56 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -58,6 +58,21 @@ sudo apt-get upgrade
 sudo apt-get dist-upgrade
 sudo ppa-purge
 sudo service redis-server restart
+sudo apt-get update 
+sudo apt-get upgrade
+ sudo apt-get install libreadline-dev libssl-dev lua5.2 liblua5.2-dev git make unzip redis-server curl libcurl4-gnutls-dev
+wget http://luarocks.org/releases/luarocks-2.2.2.tar.gz 
+ tar zxpf luarocks-2.2.2.tar.gz 
+ cd luarocks-2.2.2
+ ./configure; sudo make bootstrap 
+sudo luarocks install luasec 
+sudo luarocks install luasocket 
+sudo luarocks install redis-lua 
+sudo luarocks install lua-term 
+sudo luarocks install serpent
+sudo luarocks install dkjson 
+sudo luarocks install Lua-cURL
+cd ..
 chmod 777 tg
 chmod 777 launch.sh
 RED='\033[0;31m'
