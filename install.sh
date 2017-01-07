8ec7cc19a94d1ed1a31e0f0b0b7f166a6270916d
MutePuker <mutepuker@gmail.com>
2017-01-07 09:47:14 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -38,6 +38,39 @@ sudo apt-get install expat libexpat1-dev
 sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get dist-upgrade
+sudo apt-get install libreadline-dev
+sudo apt install libreadline-dev 
+sudo apt-get install libreadline6 
+sudo apt-get install libreadline6-dev
+sudo apt-get install libconfig-dev 
+sudo apt-get install libssl-dev 
+sudo apt-get install lua5.2 
+sudo apt-get install liblua5.2-dev 
+sudo apt-get install lua-socket 
+sudo apt-get install lua-sec 
+sudo apt-get install lua-expat 
+sudo apt-get install libevent-dev 
+sudo apt-get install make 
+sudo apt-get install unzip 
+sudo apt-get install git 
+sudo apt-get install redis-server 
+sudo apt-get install autoconf 
+sudo apt-get install g++ 
+sudo apt-get install libjansson-dev 
+sudo apt-get install libpython-dev 
+sudo apt-get install expat libexpat1-dev 
+sudo apt-get install ppa-purge 
+sudo apt-get install python3-pip 
+sudo apt-get install python3-dev
+sudo apt-get install software-properties-common
+sudo apt-get install python-software-properties
+sudo apt-get install gcc-6
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get dist-upgrade
+sudo ppa-purge
+sudo service redis-server restart
 chmod 777 tg
 chmod 777 launch.sh
 RED='\033[0;31m'
